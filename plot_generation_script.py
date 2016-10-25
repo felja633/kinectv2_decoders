7e79585bf70b04d3131b5d63d62741ec4dc3ae1c
Felix Järemo-Lawin <felja34@gitlab.ida.liu.se>
2016-10-25 14:08:47 +0200

    bugfixes in python script

--- plot_generation_script.py
+++ plot_generation_script.py
@@ -5,6 +5,7 @@ import sys
 import pylab
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
+import os.path
 
 #
 # loads float array images (424x512xtot_ims) from binary file
@@ -38,11 +39,11 @@ def classify_depth_points(depth_images, ground_truth, inlier_threshold, num_imag
     
     mask_inliers = np.zeros(sh,dtype=bool) 
     mask_outliers = np.zeros(sh,dtype=bool) 
-    print("sh = ", sh)
+    #print("sh = ", sh)
     for i in range(0,num_images):
         
         mask_inliers[:,:,i] = (np.abs(ground_truth.transpose() - depth_images[1:511,:,i]) < inlier_threshold) & (ground_truth.transpose() > 0.0)
-        mask_outliers[:,:,i] = (mask_inliers[:,:,i] != 1) & (ground_truth.transpose() > 0.0)
+        mask_outliers[:,:,i] = (mask_inliers[:,:,i] != True) & (ground_truth.transpose() > 0.0) & (depth_images[1:511,:,i]>0.0)
 
     return mask_inliers, mask_outliers
 
@@ -52,7 +53,7 @@ def classify_depth_points(depth_images, ground_truth, inlier_threshold, num_imag
 def generate_inlier_outlier_rates( max_vals_images, depth, ground_truth, inlier_threshold, num_points, num_images):
 
     sh = max_vals_images.shape
-    print(sh)
+    #print(sh)
     max_val_im = max_vals_images[:,:,1]
     sorted_max_vals = np.sort(max_val_im.ravel())
     len_max_val = len(sorted_max_vals)
@@ -60,7 +61,7 @@ def generate_inlier_outlier_rates( max_vals_images, depth, ground_truth, inlier_
     print(max_val_thresh, np.floor(len_max_val/num_points), len_max_val)
    
     num_thresh = len(max_val_thresh)
-    print("num_thresh = ", num_thresh)
+    #print("num_thresh = ", num_thresh)
     mask_inliers, mask_outliers = classify_depth_points(depth, ground_truth, inlier_threshold, num_images)
 
     num_pixels = np.count_nonzero(ground_truth)
@@ -78,11 +79,11 @@ def generate_inlier_outlier_rates( max_vals_images, depth, ground_truth, inlier_
 
     inlier_rate = np.mean(num_inliers/num_pixels,axis=0)
     outlier_rate = np.mean(num_outliers/num_pixels,axis=0)
-    inlier_rate_std = np.std(num_inliers/num_pixels,axis=0)
-    outlier_rate_std = np.std(num_outliers/num_pixels,axis=0)
+    #inlier_rate_std = np.std(num_inliers/num_pixels,axis=0)
+    #outlier_rate_std = np.std(num_outliers/num_pixels,axis=0)
     thresholds = max_val_thresh
 
-    return inlier_rate, outlier_rate, inlier_rate_std, outlier_rate_std, thresholds
+    return inlier_rate, outlier_rate, thresholds
 
 def parse_setup_xml(filename, dataset):
     tree=ET.parse(filename)
@@ -93,30 +94,29 @@ def parse_setup_xml(filename, dataset):
     pipeline_names = []
     for pipeline in root.iter('pipeline'):    
         pipeline_name = pipeline.attrib['name']
-        pipeline_names.append(pipeline_name)
+        
         setup_name = pipeline.attrib['setup_name']
 
         depth_filename = "data/"+pipeline_name+"_depth_"+setup_name+"_"+dataset+".bin"
-        depth_file_string.append(depth_filename)
         conf_filename = "data/"+pipeline_name+"_conf_"+setup_name+"_"+dataset+".bin"
-        conf_file_string.append(conf_filename)
+        if os.path.isfile(depth_filename) & os.path.isfile(depth_filename):
+            pipeline_names.append(pipeline_name)
+            depth_file_string.append(depth_filename)
+            conf_file_string.append(conf_filename)
 
     return depth_file_string, conf_file_string, pipeline_names
 
-def run_test(ground_truth, max_val_file, depth_images_file, inlier_threshold, num_points, fig_num, pipelane_name):
+def run_test(ground_truth, max_val_file, depth_images_file, inlier_threshold, num_points, fig_num, pipelane_name, marker):
     max_val_images, num_images = load_images_bin( max_val_file )
     depth_images, num_images = load_images_bin( depth_images_file )
-    #print('max_val_images ', max_val_images.shape)
-    #print('depth_images', depth_images[300,300,1])
-    print('ground_truth', ground_truth.shape)
-    
-    inlier_rate, outlier_rate, inlier_rate_std, outlier_rate_std, thresholds = generate_inlier_outlier_rates(max_val_images, depth_images, ground_truth, inlier_threshold, num_points, num_images)
 
-    print(inlier_rate)
+    inlier_rate, outlier_rate, thresholds = generate_inlier_outlier_rates(max_val_images, depth_images, ground_truth, inlier_threshold, num_points, num_images)
+    print(inlier_rate,outlier_rate)
     plt.figure(fig_num)
-    
+    plt.ylabel('inlier rate')
+    plt.xlabel('outlier rate')
     #plt.clf()
-    line = plt.plot(np.log(outlier_rate),inlier_rate,'-', label=pipelane_name)
+    line = plt.plot(np.log(outlier_rate),inlier_rate,marker, label=pipelane_name)
     first_legend = plt.legend(loc=4)
     #red_patch = mpatches.Patch(color='red', label='The red data')
     #plt.legend(handles=[red_patch])
@@ -126,12 +126,21 @@ def run_test(ground_truth, max_val_file, depth_images_file, inlier_threshold, nu
 def compare_pipelines(xml_filename, dataset):
     gt = load_gt_bin( "data/"+dataset+"_gt.bin" )
     depth_file_string, conf_file_string, pipeline_names = parse_setup_xml(xml_filename, dataset)
+    marker = '-'
     i = 0
     while i < len(depth_file_string):
-        run_test(gt, conf_file_string[i], depth_file_string[i], 300, 20, 1,pipeline_names[i])
+        num_points = 1
+        if pipeline_names[i] == 'kde':
+            num_points = 20
+        elif pipeline_names[i] == 'microsoft':
+            marker = 'k*'
+        run_test(gt, conf_file_string[i], depth_file_string[i], 300, num_points, 1,pipeline_names[i], marker)
         i += 1
-
-    plt.show()
+    
+    if len(depth_file_string) > 0:
+        plt.show()
+    else:
+        print('no files t be found :(')
 
 def visualize_frame(args):
     depth_images_file = args[1]
