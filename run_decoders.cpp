1885105a9d5179207ae785accc31c7f5136dc3fa
Felix Järemo-Lawin <felja34@gitlab.ida.liu.se>
2016-10-25 14:42:43 +0200

    only create new files if not exists

--- run_decoders.cpp
+++ run_decoders.cpp
@@ -10,17 +10,16 @@
 
 int main(int argc, char** argv)
 {
-    if(argc != 4)
+    if(argc != 3)
     {
-        std::cout<<"usage:\n ./kinectv2_decoders /path/to/convig/setup.xml dataset /path/to/dataset/";
+        std::cout<<"usage:\n ./kinectv2_decoders /path/to/convig/setup.xml dataset";
         return 0;
     }
 
     std::string setup_xmlfile = argv[1];
     std::string data_set = argv[2];
-    std::string data_set_path = argv[3];
 
-    std::string data_file_name = data_set_path;
+    std::string data_file_name = "../logs/";
     data_file_name.append(data_set);
     data_file_name.append(".h5");
 
@@ -46,49 +45,80 @@ int main(int argc, char** argv)
     {
         if(strcmp(params[i].pipeline.c_str(),"libfreenect2") == 0)
         {
-            //initialize libreenect2 decoder
-            CpuDepthPacketProcessor* libfreenect2 = new CpuDepthPacketProcessor();
-            libfreenect2->initParameters(params[i]);
-            libfreenect2->loadP0TablesFromCommandResponse(p0_table, len_p0_table);
-            libfreenect2->loadXZTables(&dev.xtable[0], &dev.ztable[0]);
-            libfreenect2->loadLookupTable(&dev.lut[0]);
-            processors.push_back(libfreenect2);
+		        std::string depth_filename = "../data/";
+						depth_filename.append(params[i].pipeline);
+				    std::string conf_filename = "../data/";
+						conf_filename.append(params[i].pipeline);
+        
+				    depth_filename.append("_depth_");
+				    depth_filename.append(params[i].setup_name);
+				    depth_filename.append("_");
+				    depth_filename.append(data_set);
+						depth_filename.append(".bin");
 
+				    conf_filename.append("_conf_");
+				    conf_filename.append(params[i].setup_name);        
+				    conf_filename.append("_");
+				    conf_filename.append(data_set);
+						conf_filename.append(".bin");
+				
+						if(!(std::ifstream(depth_filename.c_str()) || std::ifstream(conf_filename.c_str())))
+						{
+						  std::fstream* depth_file = new std::fstream(depth_filename.c_str(), std::ios::out | std::ios::binary | std::ios::app);
+						  std::fstream* conf_file = new std::fstream(conf_filename.c_str(), std::ios::out | std::ios::binary | std::ios::app);
+						  depth_files.push_back(depth_file);
+						  conf_files.push_back(conf_file);
+            //initialize libreenect2 decoder
+		          CpuDepthPacketProcessor* libfreenect2 = new CpuDepthPacketProcessor();
+		          libfreenect2->initParameters(params[i]);
+		          libfreenect2->loadP0TablesFromCommandResponse(p0_table, len_p0_table);
+		          libfreenect2->loadXZTables(&dev.xtable[0], &dev.ztable[0]);
+		          libfreenect2->loadLookupTable(&dev.lut[0]);
+		          processors.push_back(libfreenect2);
+						}
         }
         else if(strcmp(params[i].pipeline.c_str(),"kde") == 0)
         {
             //initialize kde decoder
-            CpuKdeDepthPacketProcessor* kde = new CpuKdeDepthPacketProcessor();
-            kde->initParameters(params[1]);
-            kde->loadP0TablesFromCommandResponse(p0_table, len_p0_table);
-            kde->loadXZTables(&dev.xtable[0], &dev.ztable[0]);
-            kde->loadLookupTable(&dev.lut[0]);
-            processors.push_back(kde);
-        }
+      
+
+		        std::string depth_filename = "../data/";
+						depth_filename.append(params[i].pipeline);
+				    std::string conf_filename = "../data/";
+						conf_filename.append(params[i].pipeline);
+        
+				    depth_filename.append("_depth_");
+				    depth_filename.append(params[i].setup_name);
+				    depth_filename.append("_");
+				    depth_filename.append(data_set);
+						depth_filename.append(".bin");
+
+				    conf_filename.append("_conf_");
+				    conf_filename.append(params[i].setup_name);        
+				    conf_filename.append("_");
+				    conf_filename.append(data_set);
+						conf_filename.append(".bin");
+				
+						if(!(std::ifstream(depth_filename.c_str()) || std::ifstream(conf_filename.c_str())))
+						{
+						  std::fstream* depth_file = new std::fstream(depth_filename.c_str(), std::ios::out | std::ios::binary | std::ios::app);
+						  std::fstream* conf_file = new std::fstream(conf_filename.c_str(), std::ios::out | std::ios::binary | std::ios::app);
+						  depth_files.push_back(depth_file);
+						  conf_files.push_back(conf_file);
+
+				      CpuKdeDepthPacketProcessor* kde = new CpuKdeDepthPacketProcessor();
+		          kde->initParameters(params[1]);
+		          kde->loadP0TablesFromCommandResponse(p0_table, len_p0_table);
+		          kde->loadXZTables(&dev.xtable[0], &dev.ztable[0]);
+		          kde->loadLookupTable(&dev.lut[0]);
+							processors.push_back(kde);
+						}   
+		    }
         else
         {
             std::cout<<"pipeline "<<params[i].pipeline<<" not implemented\n";
             continue;
         }
-        std::string depth_filename = "../data/";
-				depth_filename.append(params[i].pipeline);
-        std::string conf_filename = "../data/";
-				conf_filename.append(params[i].pipeline);
-        
-        depth_filename.append("_depth_");
-        depth_filename.append(params[i].setup_name);
-        depth_filename.append("_");
-        depth_filename.append(data_set);
-
-        conf_filename.append("_conf_");
-        conf_filename.append(params[i].setup_name);        
-        conf_filename.append("_");
-        conf_filename.append(data_set);
-
-        std::fstream* depth_file = new std::fstream(depth_filename.append(".bin").c_str(), std::ios::out | std::ios::binary | std::ios::app);
-        std::fstream* conf_file = new std::fstream(conf_filename.append(".bin").c_str(), std::ios::out | std::ios::binary | std::ios::app);
-        depth_files.push_back(depth_file);
-        conf_files.push_back(conf_file);
     }
 
     //decode and save frames to file
